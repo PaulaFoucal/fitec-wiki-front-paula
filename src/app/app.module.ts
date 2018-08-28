120644c8d183b4936529bb8e9fc36db375af047a
PaulaFoucal <paula.foucal@gmail.com>
2018-08-28 10:40:13 +0200

    buttons and layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ArticleComponent } from './article/article.component';
+import { HeaderComponent } from './shared/layout/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    ArticleComponent
+    ArticleComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
